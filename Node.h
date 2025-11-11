102110352dbdb03b1cf4cc2cc8f8823ae63a13d1
Dani <dgonfue@upv.edu.es>
2025-11-11 11:27:57 +0100

    Añadida implementación de la clase Node

---
+++ Node.h
@@ -0,0 +1,25 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <ostream>
+#include <stdexcept>
+
+template <typename T>
+class Node {
+	public:
+	    T data;
+	    Node<T>* next;
+
+	    // Constructor
+            Node(T data, Node<T>* next = nullptr)
+            : data(data), next(next) {}
+		    
+            // Imprimir sólo el dato
+            friend std::ostream& operator<<(std::ostream &out, const Node<T> &node) {
+            out << node.data;
+            return out;
+            }
+            };
+			    
+            #endif
+			                                            
